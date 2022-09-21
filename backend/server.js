c31575c576bb1cb5b597306d41abda015898416f
Hardik <rajputhardik12@gmail.com>
2022-09-21 21:33:30 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const workoutRoute = require("./routes/workout");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", workoutRoute);
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
