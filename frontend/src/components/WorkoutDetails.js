7ceb4ce171defaeb8997e3cf993040f0065e2158
Hardik <rajputhardik12@gmail.com>
2022-09-21 23:39:14 +0530

    Delete

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -3,9 +3,12 @@ import formatDistancetoNow from "date-fns/formatDistanceToNow";
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext();
   const handleClick = async () => {
-    const response = await fetch("/api/workouts/" + workout._id, {
-      method: "DELETE",
-    });
+    const response = await fetch(
+      "https://workoutjournal.onrender.com/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
 
     const json = await response.json();
     if (response.ok) {
